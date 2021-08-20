41abd5561aeb86424d0f9d30bf9ea5491fa7c38e
Hamza Hadfi <01101000.hedfi@gmail.com>
2021-08-21 00:26:20 +0100

    Add doc

--- AHClaim/Record/FileTrailerRecord.cs
+++ AHClaim/Record/FileTrailerRecord.cs
@@ -2,12 +2,54 @@
 
 namespace AHClaim.Record
 {
+    /// <summary>
+    /// File Trailer Record
+    /// </summary>
     [FixedLengthRecord]
     public class FileTrailerRecord
     {
-        [FieldFixedLength(2)] public string TrailerRecordIndicator { get; set; }
-        [FieldFixedLength(9)] public string ClaimCount { get; set; }
-        [FieldFixedLength(9)] public string MicClaimCount { get; set; }
-        [FieldFixedLength(6)] public string FileTrailerRecordFutureUse { get; set; }
+        /// <summary>
+        /// Field #: 1
+        /// Field Name: Trailer Record Indicator ($T)
+        /// Format: AN 
+        /// Length: 2
+        /// Values:
+        /// Definition:
+        /// </summary>
+        [FieldFixedLength(2)]
+        public string TrailerRecordIndicator { get; set; }
+
+        /// <summary>
+        /// Field #: 2
+        /// Field Name: Claim Count
+        /// Format: N
+        /// Length: 9
+        /// Values:
+        /// Definition:
+        /// </summary>
+        [FieldFixedLength(9)]
+        public string ClaimCount { get; set; }
+
+        /// <summary>
+        /// Field #: 3
+        /// Field Name: MIC Claim Count
+        /// Format: N 
+        /// Length: 9
+        /// Values:
+        /// Definition:
+        /// </summary>
+        [FieldFixedLength(9)]
+        public string MicClaimCount { get; set; }
+
+        /// <summary>
+        /// Field #: 4
+        /// Field Name: Future Use
+        /// Format:
+        /// Length: 6
+        /// Values:
+        /// Definition:
+        /// </summary>
+        [FieldFixedLength(6)]
+        public string FileTrailerRecordFutureUse { get; set; }
     }
 }
