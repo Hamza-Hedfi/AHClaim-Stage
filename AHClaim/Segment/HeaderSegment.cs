1560af97d8c2fadd15387773fc16c4a04e4c73a1
Hamza Hadfi <01101000.hedfi@gmail.com>
2021-08-21 00:41:28 +0100

    Add HeaderSegment.cs

---
+++ AHClaim/Segment/HeaderSegment.cs
@@ -0,0 +1,9 @@
+﻿namespace AHClaim.Segment
+{
+    public class HeaderSegment
+    {
+        public string ClaimFileRecordIndicator { get; set; }
+        public string VersionNumber { get; set; }
+        public string MinorVersionNumber { get; set; }
+    }
+}
