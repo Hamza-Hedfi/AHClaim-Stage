0335286df5e2dfcc0a969a1470cd73970adf6d96
Hamza Hadfi <01101000.hedfi@gmail.com>
2021-08-22 21:01:05 +0100

    Add doc. for Claim Reject Segment properties

--- AHClaim/Record/RawClaimRecord.cs
+++ AHClaim/Record/RawClaimRecord.cs
@@ -1471,18 +1471,125 @@ namespace AHClaim.Record
 
         #region Claim Reject Segment
 
-        [FieldFixedLength(1)] public string ClaimRejectSegmentSeparatorCharacter { get; set; }
-        [FieldFixedLength(2)] public string ClaimRejectSegmentIdentifier { get; set; }
-        [FieldFixedLength(3)] public string ClaimRejectSegmentRejectCode1 { get; set; }
-        [FieldFixedLength(3)] public string ClaimRejectSegmentRejectCode2 { get; set; }
-        [FieldFixedLength(3)] public string ClaimRejectSegmentRejectCode3 { get; set; }
-        [FieldFixedLength(3)] public string ClaimRejectSegmentRejectCode4 { get; set; }
-        [FieldFixedLength(3)] public string ClaimRejectSegmentRejectCode5 { get; set; }
-        [FieldFixedLength(3)] public string ClaimRejectSegmentEccRejectCode1 { get; set; }
-        [FieldFixedLength(3)] public string ClaimRejectSegmentEccRejectCode2 { get; set; }
-        [FieldFixedLength(3)] public string ClaimRejectSegmentEccRejectCode3 { get; set; }
-        [FieldFixedLength(3)] public string ClaimRejectSegmentEccRejectCode4 { get; set; }
-        [FieldFixedLength(3)] public string ClaimRejectSegmentEccRejectCode5 { get; set; }
+        /// <summary>
+        /// Firld #: 1 
+        /// Field Name: Segment Separator Character 
+        /// Format: AN 
+        /// Length: 1
+        /// Value: >
+        /// </summary>
+        [FieldFixedLength(1)]
+        public string ClaimRejectSegmentSeparatorCharacter { get; set; }
+
+        /// <summary>
+        /// Firld #: 2 
+        /// Field Name: Segment Identifier ( RJ ) 
+        /// Format: AN 
+        /// Length: 2
+        /// Value: RJ
+        /// </summary>
+        [FieldFixedLength(2)]
+        public string ClaimRejectSegmentIdentifier { get; set; }
+
+        /// <summary>
+        /// Firld #: 3 
+        /// Field Name: Reject Code 1 
+        /// Format: AN 
+        /// Length: 3
+        /// Value: NCPDP Rejection Code
+        /// </summary>
+        [FieldFixedLength(3)]
+        public string ClaimRejectSegmentRejectCode1 { get; set; }
+
+        /// <summary>
+        /// Firld #: 4 
+        /// Field Name: Reject Code 2 
+        /// Format: AN 
+        /// Length: 3
+        /// Definition: NCPDP Rejection Code 
+        /// </summary>
+        [FieldFixedLength(3)]
+        public string ClaimRejectSegmentRejectCode2 { get; set; }
+
+        /// <summary>
+        /// Firld #: 5 
+        /// Field Name: Reject Code 3 
+        /// Format: AN 
+        /// Length: 3
+        /// Definition:  NCPDP Rejection Code 
+        /// </summary>
+        [FieldFixedLength(3)]
+        public string ClaimRejectSegmentRejectCode3 { get; set; }
+
+        /// <summary>
+        /// Firld #: 6 
+        /// Field Name: Reject Code 4 
+        /// Format: AN 
+        /// Length: 3
+        /// Definition:  NCPDP Rejection Code 
+        /// </summary>
+        [FieldFixedLength(3)]
+        public string ClaimRejectSegmentRejectCode4 { get; set; }
+
+        /// <summary>
+        /// Firld #: 7 
+        /// Field Name: Reject Code 5 
+        /// Format: AN 
+        /// Length: 3
+        /// Definition:  NCPDP Rejection Code 
+        /// </summary>
+        [FieldFixedLength(3)]
+        public string ClaimRejectSegmentRejectCode5 { get; set; }
+
+        /// <summary>
+        /// Firld #: 8 
+        /// Field Name: ECC Reject Code 
+        /// Format: AN 
+        /// Length: 3
+        /// Definition:  Error clarification code for Rej Code 1 
+        /// </summary>
+        [FieldFixedLength(3)]
+        public string ClaimRejectSegmentEccRejectCode1 { get; set; }
+
+        /// <summary>
+        /// Firld #: 9 
+        /// Field Name: ECC Reject Code 
+        /// Format: AN 
+        /// Length: 3
+        /// Definition:  Error clarification code for Rej Code 2 
+        /// </summary>
+        [FieldFixedLength(3)]
+        public string ClaimRejectSegmentEccRejectCode2 { get; set; }
+
+        /// <summary>
+        /// Firld #: 10
+        /// Field Name:  ECC Reject Code 
+        /// Format: AN 
+        /// Length: 3
+        /// Definition:  Error clarification code for Rej Code 3 
+        /// </summary>
+        [FieldFixedLength(3)]
+        public string ClaimRejectSegmentEccRejectCode3 { get; set; }
+
+        /// <summary>
+        /// Firld #: 11
+        /// Field Name:  ECC Reject Code 
+        /// Format: AN 
+        /// Length: 3
+        /// Definition:  Error clarification code for Rej Code 4 
+        /// </summary>
+        [FieldFixedLength(3)]
+        public string ClaimRejectSegmentEccRejectCode4 { get; set; }
+
+        /// <summary>
+        /// Firld #: 12
+        /// Field Name:  ECC Reject Code 
+        /// Format: AN 
+        /// Length: 3
+        /// Definition: Error clarification code for Rej Code 5
+        /// </summary>
+        [FieldFixedLength(3)]
+        public string ClaimRejectSegmentEccRejectCode5 { get; set; }
 
         #endregion
 
