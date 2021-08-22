cabbf8e83ae7370b01ec6a3cc0fe19bbade8df71
Hamza Hadfi <01101000.hedfi@gmail.com>
2021-08-22 23:49:35 +0100

    Add SubmittedDurOverrideSegment.cs definition

--- AHClaim/Segment/SubmittedDurOverrideSegment.cs
+++ AHClaim/Segment/SubmittedDurOverrideSegment.cs
@@ -2,6 +2,139 @@
 {
     public class SubmittedDurOverrideSegment
     {
-        // TODO
+        /// <summary>
+        /// Field #: 3
+        /// Field Name: Reason for Service (Conflict) 1 
+        /// Format: AN
+        /// Length: 2 
+        /// Definition: NCPDP Reason for Service submitted by pharmacy
+        /// </summary>
+        public string ReasonForServiceConflict1 { get; set; }
+
+        /// <summary>
+        /// Field #: 4
+        /// Field Name: Professional Service Code (Intervention) 1 
+        /// Format: AN
+        /// Length: 2 
+        /// Definition: NCPDP Profess Service Code submitted by pharmacy
+        /// </summary>
+        public string ProfessionalServiceCodeIntervention1 { get; set; }
+
+        /// <summary>
+        /// Field #: 5
+        /// Field Name: Result of Service Code (Outcome) 1 
+        /// Format: AN
+        /// Length: 2 
+        /// Definition: NCPDP Result of Service Code submitted by pharmacy
+        /// </summary>
+        public string ResultOfServiceCodeOutcome1 { get; set; }
+
+        /// <summary>
+        /// Field #: 6
+        /// Field Name: Reason for Service (Conflict) 2 
+        /// Format: AN
+        /// Length: 2 
+        /// Definition: NCPDP Reason for Service submitted by pharmacy
+        /// </summary>
+        public string ReasonForServiceConflict2 { get; set; }
+
+        /// <summary>
+        /// Field #: 7
+        /// Field Name: Professional Service Code (Intervention) 2 
+        /// Format: AN
+        /// Length: 2 
+        /// Definition: NCPDP Profess Service Code submitted by pharmacy
+        /// </summary>
+        public string ProfessionalServiceCodeIntervention2 { get; set; }
+
+        /// <summary>
+        /// Field #: 8
+        /// Field Name: Result of Service Code (Outcome) 2 
+        /// Format: AN
+        /// Length: 2 
+        /// Definition: NCPDP Result of Service Code submitted by pharmacy
+        /// </summary>
+        public string ResultOfServiceCodeOutcome2 { get; set; }
+
+        /// <summary>
+        /// Field #: 9
+        /// Field Name: Reason for Service (Conflict) 3 
+        /// Format: AN
+        /// Length: 2 
+        /// Definition: NCPDP Reason for Service submitted by pharmacy
+        /// </summary>
+        public string ReasonForServiceConflict3 { get; set; }
+
+        /// <summary>
+        /// Field #: 10
+        /// Field Name: Professional Service Code (Intervention) 3 
+        /// Format: AN
+        /// Length: 2 
+        /// Definition: NCPDP Profess Service Code submitted by pharmacy
+        /// </summary>
+        public string ProfessionalServiceCodeIntervention3 { get; set; }
+
+        /// <summary>
+        /// Field #: 11
+        /// Field Name: Result of Service Code (Outcome) 3 
+        /// Format: AN
+        /// Length: 2 
+        /// Definition: NCPDP Result of Service Code submitted by pharmacy
+        /// </summary>
+        public string ResultOfServiceCodeOutcome3 { get; set; }
+
+        /// <summary>
+        /// Field #: 12
+        /// Field Name: Reason for Service (Conflict) 4 
+        /// Format: AN
+        /// Length: 2 
+        /// Definition: NCPDP Reason for Service submitted by pharmacy
+        /// </summary>
+        public string ReasonForServiceConflict4 { get; set; }
+
+        /// <summary>
+        /// Field #: 13
+        /// Field Name: Professional Service Code (Intervention) 4 
+        /// Format: AN
+        /// Length: 2 
+        /// Definition: NCPDP Profess Service Code submitted by pharmacy
+        /// </summary>
+        public string ProfessionalServiceCodeIntervention4 { get; set; }
+
+        /// <summary>
+        /// Field #: 14
+        /// Field Name: Result of Service Code (Outcome) 4 
+        /// Format: AN
+        /// Length: 2 
+        /// Definition: NCPDP Result of Service Code submitted by pharmacy
+        /// </summary>
+        public string ResultOfServiceCodeOutcome4 { get; set; }
+
+        /// <summary>
+        /// Field #: 15
+        /// Field Name: Reason for Service (Conflict) 5 
+        /// Format: AN
+        /// Length: 2 
+        /// Definition: NCPDP Reason for Service submitted by pharmacy
+        /// </summary>
+        public string ReasonForServiceConflict5 { get; set; }
+
+        /// <summary>
+        /// Field #: 16
+        /// Field Name: Professional Service Code (Intervention) 5 
+        /// Format: AN
+        /// Length: 2 
+        /// Definition: NCPDP Profess Service Code submitted by pharmacy
+        /// </summary>
+        public string ProfessionalServiceCodeIntervention5 { get; set; }
+
+        /// <summary>
+        /// Field #: 17
+        /// Field Name: Result of Service Code (Outcome) 5 
+        /// Format: AN
+        /// Length: 2 
+        /// Definition: NCPDP Result of Service Code submitted by pharmacy
+        /// </summary>
+        public string ResultOfServiceCodeOutcome5 { get; set; }
     }
 }
